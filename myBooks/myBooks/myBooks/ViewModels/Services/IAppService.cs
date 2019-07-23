2b62ef08b2b0928618799880f989ca0a1d8ec185
Gabriel Dias <gabriel.souzadias@hotmail.com>
2019-07-22 22:13:15 -0400

    Http Requests
    #Livros
    -GetBooks
    -GetBooksById
    -PostBook

---
+++ myBooks/myBooks/myBooks/ViewModels/Services/IAppService.cs
@@ -0,0 +1,22 @@
+﻿using myBooks.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace myBooks.ViewModels.Services
+{
+    public interface IAppService
+    {
+        #region BooksRequest
+        //Get
+        Task<List<Livro>> GetBooks();
+        //GetById
+        Task<Livro> GetBookById(int id);
+        //Post
+        Task PostBook(Usuario usuario, Livro livro);
+        //Put
+        Task PutBook();
+        //Delete
+        Task DeleteBook();
+        #endregion
+    }
+}
---
+++ myBooks/myBooks/myBooks/Views/Services/AppService.cs
@@ -0,0 +1,84 @@
+﻿using myBooks.Models;
+using myBooks.ViewModels.Services;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myBooks.Views.Services
+{
+    public class AppService : IAppService
+    {
+        private HttpClient client;
+        private readonly string booksRequestUri = "https://localhost:44326/api/Livros";
+
+        public AppService()
+        {
+            client = new HttpClient();
+        }
+
+        #region BooksRequest
+        public async Task<List<Livro>> GetBooks()
+        {
+            HttpResponseMessage responseMessage = await client.GetAsync(booksRequestUri);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                string content = await responseMessage.Content.ReadAsStringAsync();
+                List<Livro> livros = JsonConvert.DeserializeObject<List<Livro>>(content);
+                return livros;
+            }
+            return null;
+        }
+
+        public async Task<Livro> GetBookById(int id)
+        {
+            HttpResponseMessage responseMessage = await client.GetAsync(booksRequestUri + id.ToString());
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                string content = await responseMessage.Content.ReadAsStringAsync();
+                Livro livro = JsonConvert.DeserializeObject<Livro>(content);
+                return livro;
+            }
+            return null;
+        }
+
+
+        public async Task PostBook(Usuario usuario, Livro livro)
+        {
+            Livro _livro = new Livro()
+            {
+                BookImgName = livro.BookImgName,
+                BookImgVb = livro.BookImgVb,
+                Title = livro.Title,
+                Year = livro.Year,
+                Publisher = livro.Publisher,
+                Genre = livro.Genre,
+                UserId = usuario.UserId
+            };
+
+            string livroJson = JsonConvert.SerializeObject(_livro);
+
+            HttpContent httpContent = new StringContent(livroJson, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage responseMessage = client.PostAsync(booksRequestUri, httpContent).Result;
+
+            responseMessage.EnsureSuccessStatusCode();
+
+            string json = await responseMessage.Content.ReadAsStringAsync();
+        }
+
+        public Task PutBook()
+        {
+            throw new NotImplementedException();
+        }
+        public Task DeleteBook()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
